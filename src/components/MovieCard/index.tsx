eda0c1ebc68aca37669bc696f9077e0c0b201a07
01samuelgomx <SamuelOliveirat20hl@gmail.com>
2024-08-23 17:56:51 -0300

    :art:Estilizando e melhorando o layout com grid

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,5 +1,7 @@
 import { Movie } from "@/types/movie";
 import Star from "../Star";
+import "../scss/index.scss";
+
 
 export interface Props {
   movie: Movie;
@@ -20,13 +22,22 @@ export default function MovieCard(props: Props) {
 
       <div className="movieInfo">
         <h2 className="title">{movie.title}</h2>
+
+        {movie.vote_average > 0 &&
         
         <Star 
           rating={movie.vote_average}
         />
+        
+        }
 
         <div className="hiddenContent">
-          <p className="description">{movie.overview}</p>
+          {movie.overview &&
+          <p className="description">{movie.overview.length > 100
+            ?`${movie.overview.substring(0, 100)}...`
+            :movie.overview
+          }</p>
+        }
           <button className="btn">Ver mais</button>
         </div>
       </div>
